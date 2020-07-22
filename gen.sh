e28b4f1720f2287aeb6734e884d7ffcb811607f3
Yichen Xu <yichen.x@outlook.com>
2020-07-22 20:39:16 +0800

    Change target language for better support

--- gen.sh
+++ gen.sh
@@ -1,6 +1,6 @@
 echo 'Generating verilog sources ...'
 mkdir -p gen
 cd gen
-clash --systemverilog ../src/Cache.hs -i../src/
+clash --verilog ../src/Cache.hs -i../src/
 rm ../src/**/*.{dyn_o,dyn_hi,hi,o}
 rm ../src/*.{dyn_o,dyn_hi,hi,o}
