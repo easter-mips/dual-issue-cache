6a8a2250274d9d1b40f2797df4729e82913d441c
Yichen Xu <yichen.x@outlook.com>
2020-07-22 21:23:43 +0800

    Patch timescale automatically

--- gen.sh
+++ gen.sh
@@ -4,3 +4,12 @@ cd gen
 clash --verilog ../src/Cache.hs -i../src/
 rm ../src/**/*.{dyn_o,dyn_hi,hi,o}
 rm ../src/*.{dyn_o,dyn_hi,hi,o}
+
+# patch timescale and generate target
+cd verilog/Cache/icache_controller/
+patch -u icache_controller.v -i ./../../../../utils/patch/timescale.patch
+
+cd ../../../../
+mkdir -p target
+cp gen/verilog/Cache/icache_controller/*.v target/
+
