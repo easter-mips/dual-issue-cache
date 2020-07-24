0038bf490ba871f2b595a36de8401925a7d94e77
Yichen Xu <yichen.x@outlook.com>
2020-07-24 18:47:40 +0800

    Use awk to patch timescale

--- gen.sh
+++ gen.sh
@@ -1,15 +1,25 @@
 echo 'Generating verilog sources ...'
+
 mkdir -p gen
 cd gen
-clash --verilog ../src/Cache.hs -i../src/
+
+for GEN_TOP in ICacheTop DCacheAgentTop
+do
+    echo "Compiling ${GEN_TOP} ..."
+    clash --verilog ../src/${GEN_TOP}.hs -i../src/
+done
 rm ../src/**/*.{dyn_o,dyn_hi,hi,o}
 rm ../src/*.{dyn_o,dyn_hi,hi,o}
 
 # patch timescale and generate target
-cd verilog/Cache/icache_controller/
-patch -u icache_controller.v -i ./../../../../utils/patch/timescale.patch
-
-cd ../../../../
+cd ..
+rm -rf target
 mkdir -p target
-cp gen/verilog/Cache/icache_controller/*.v target/
-
+for p in $(find . -name '*.v')
+do
+    echo "patching ${p}"
+    awk '{ gsub("100fs/100fs", "1ns / 1ps"); print $0 }' ${p} > ${p}.patched
+    rm ${p}
+    mv ${p}.patched ${p}
+    cp ${p} ./target/
+done
